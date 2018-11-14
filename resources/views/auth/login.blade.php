1d37ea607b7610ffd8690e6d5a50690935c4aa67
unknown <christophe.lignac33@gmail.com>
2018-11-14 22:25:25 +0100

    Ajout Interface Creation User et Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,95 @@
 @extends('layouts.app')
-
+@section('pageTitle', 'Page Title')
 @section('content')
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>App Name - @yield('title')</title>
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+        <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <!-- <audio autoplay>
+            <source src="http://s2.3lbh.com/s/87EJ97gi.mp3" type="audio/mpeg">
+            Votre navigateur ne supporte pas le son de cette page.
+        </audio> -->
+        <!-- Start Main Container -->
+        <div class="main-container">
+            <!-- Start Pokemon Ball Top Part -->
+            <div class="pokemon-top-part"></div>
+            <!-- End Pokemon Ball Top Part -->
+            <!-- Start Main Forms -->
+            <div class="main-forms">
+                <div class="signup-form">
+                    <form class="sign-back" method="POST" action="{{ route('login') }}">
+                    @csrf
+                        <h1>Me connecter</h1>
+                        <div class="signup-row">
+                            <i class="fa fa-user"></i>
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                            @if ($errors->has('email'))
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+
+                        <div class="signup-row">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                        </div>
+
+                        <div class="form-bottom">
+                                <div class="form-info">
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" value="{{ old('remember') ? 'checked' : '' }}">
+                                    <label class="form-check-label" for="remember">
+                                        {{ __('Memoriser') }}
+                                    </label>
+                                </div>
+                                <div class="form-info">
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Mot de passe oublié ?') }}
+                                    </a>
+                                </div>
+                                <div class="form-info">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Connexion') }}
+                                    </button>
+                                </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- End Main Forms -->
+            <!-- Start Pokemon Ball Bottom Part -->
+            <div class="pokemon-bottom-part">
+                <div class="white-part"></div>
+                <div class="black-line"></div>
+            </div>
+            <!-- End Pokemon Ball Bottom Part -->
+        </div>
+        <!-- End Main Container -->
+        <!-- Start Scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="{{asset('js/login.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/TweenMax.min.js"></script>
+<!--         <script src="https://use.fontawesome.com/7dddae9ad9.js"></script> -->
+<!--         <script type="text/javascript" src="main.js"></script> -->
+        <!-- End Scripts -->
+    </body>
+</html>
+
+@endsection
+<!-- @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Se connecter') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -68,4 +152,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection -->
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,89 @@
 @extends('layouts.app')
 
 @section('content')
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+        <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <!-- Start Main Container -->
+        <div class="main-container">
+            <!-- Start Pokemon Ball Top Part -->
+            <div class="pokemon-top-part"></div>
+            <!-- End Pokemon Ball Top Part -->
+            <!-- Start Main Forms -->
+            <div class="main-forms">
+                <div class="signup-form">
+                    <form class="sign-back" method="POST" action="form-control{{ route('register') }}">
+                        @csrf
+                        <h1>Creer un compte</h1>
+
+                        <div class="signup-row">
+                            <i class="fa fa-user"></i>
+                                <input id="name" type="text" placeholder="PSEUDO" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                        </div>
+                        <div class="signup-row">
+                            <i class="fa fa-envelope-o"></i>
+                                <input id="email" type="email" placeholder="ADRESSE MAIL" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                        </div>
+
+                        <div class="signup-row">
+                        <i class="fa fa-key"></i>
+                                <input id="password" type="password" placeholder="MOT DE PASSE" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                        </div>
+                        <div class="signup-row">
+                        <i class="fa fa-key"></i>
+                                <input id="password-confirm" type="password" placeholder="CONFIRMATION" class="form-control" name="password_confirmation" required>
+                        </div>
+                        <div class="form-bottom">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Enregistrer') }}
+                                </button>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+            <!-- End Main Forms -->
+            <!-- Start Pokemon Ball Bottom Part -->
+            <div class="pokemon-bottom-part">
+                <div class="white-part"></div>
+                <div class="black-line"></div>
+            </div>
+            <!-- End Pokemon Ball Bottom Part -->
+        </div>
+        <!-- End Main Container -->
+        <!-- Start Scripts -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="{{asset('js/register.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.2/TweenMax.min.js"></script>
+<!--         <script src="https://use.fontawesome.com/7dddae9ad9.js"></script> -->
+<!--         <script type="text/javascript" src="main.js"></script> -->
+        <!-- End Scripts -->
+    </body>
+</html>
+
+@endsection
+<!-- @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -74,4 +157,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection -->
